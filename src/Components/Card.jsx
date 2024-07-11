ae57bb9c50952eb08ea7e9234f7c319ba93d0539
Jefferson De Andrade <jeffersonandrade1999@gmail.com>
2024-07-11 17:32:12 -0300

    STYLE: tela de seleção de Deck disponível

---
+++ src/Components/Card.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Card = () => {
+    return (
+        <div className=" shadow-md card-menu border flex items-center justify-center hover:bg-slate-500 cursor-pointer m-2 green-border">
+          <span className='text-4xl font-bold text-white green-text'>+</span>
+        </div>
+    );
+}
+
+export default Card;
--- src/routes/MenuInicial.jsx
+++ src/routes/MenuInicial.jsx
@@ -1,31 +1,29 @@
 import React from 'react'
+import Card from '../Components/Card'
 
 function MenuInicial() {
   return (
-    <div className='min-h-screen flex justify-center items-center bg-marrom-escuro'>
-      <main className='container flex justify-center'>
+    
+
+    <div className=" bg-[url('./midias/bg_login.jpeg')] bg-cover bg-center bg-no-repeat min-h-screen flex justify-center items-center">
         
-        <div className='w-96 h-96 rounded-full border flex items-center justify-center'>
-          <ul className='w-80 h-80 rounded-full border flex items-center justify-center flex-wrap bg-marrom-claro'>
-            <div className='h-36 w-3/4 flex flex-wrap justify-center items-center'>
-              <li 
-                className='w-3/4 smt-8 h-8 text-center text-xl font-bold px-2 formato bg-marrom-escuro
-                cursor-pointer hover:bg-white'>
-              Jogar</li>
-              <li 
-                className='w-full mt-8 h-8 text-center text-xl font-bold px-2 formato bg-marrom-escuro
-                cursor-pointer hover:bg-white'>
-              Jogar</li>
-              <li 
-                className='w-3/4 mt-8 h-8 text-center text-xl font-bold px-2 formato bg-marrom-escuro cursor-pointer hover:bg-white'>
-              Jogar</li>
-
-            </div>
+        <div className='sm:w-full sm:mx-2 md:w-3/4 bg-preto min-h-screen p-4'>
+          <h1 className='text-2xl font-bold text-center mb-4 green-text'>Seus decks</h1>
+          <p className='mb-4 text-xl font-semibold'>Aqui é onde você irá montar seus primeiros Decks, o passo a passo é bem simples: </p>
+          <ul className='list-disc px-4 mb-8'>
+            <li className='mb-1'>Clique no ícone de + ou no quadrado abaixo.</li>
+            <li className='mb-1'>Selecione a arte e dê um nome para o seu Deck.</li>
+            <li className='mb-1'>Após isso você será redirecionado ao menu de seleção de cartas, dentro desse menu basta você clicar na carta selecionada e ela irá aparecer no menu a direita.</li>
+            <li className='mb-1'> <span className='text-red-600 font-bold'>IMPORTANTE:</span> não esqueça de salvar seu Deck após selecionar todas as cartas que deseja.</li>
           </ul>
+          <div className='w-full flex justify-evenly flex-wrap'>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
+          </div>
         </div>
-         
 
-      </main>
     </div>
   )
 }
